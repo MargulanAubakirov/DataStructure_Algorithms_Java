350e4de299e73d66eccc8cf76ec0eaa270ea2198
MargulanAubakirov <nalugram@gmail.com>
2020-09-09 20:09:36 -0500

    insertSort, selectSort, shellSort

--- src/selectionsort/Main.java
+++ src/selectionsort/Main.java
@@ -1,22 +1,27 @@
-package bubblesort;
+package selectionsort;
 
 public class Main {
     public static void main(String[] args) {
 
         int[] intArray = {20,35,-15,7,55,1,-22};
 
-        for (int lastUnsortedIndex = intArray.length-1; lastUnsortedIndex > 0 ; lastUnsortedIndex--) {
-            for (int i = 0; i < lastUnsortedIndex; i++) {
-                if (intArray[i]>intArray[i+1]){
-                    swap(intArray, i, i+1);
+        for (int lastUnsortedIndex = intArray.length-1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex ; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest =i;
                 }
             }
-
+            swap(intArray, largest, lastUnsortedIndex);
         }
+
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
-            
+
         }
+
 
     }
     public static void swap(int[] array, int i, int j){
@@ -29,5 +34,3 @@ public class Main {
         array[j]=temp;
     }
 }
-
-
